cf75d409a5455c482923b3e83c4cd14d1dfb8c21
fidelis <fidelisnwabisi@gmail.com>
2023-07-30 10:35:09 +0100

    added _icons and made it clicable with _buildIcon

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,19 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+  final List<IconData> _icons = [
+    FontAwesomeIcons.plane,
+    FontAwesomeIcons.bed,
+    FontAwesomeIcons.personWalking,
+    FontAwesomeIcons.personBiking,
+  ];
+
+  _buildIcon(int index) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _selectedIndex = index;
+        });
+        // TODO: inmplement the code/logic to change the list
+        //view builder in destination carosel here
+      },
+      child: Container(
+        height: 60,
+        width: 60,
+        decoration: BoxDecoration(
+          color: _selectedIndex == index
+              ? Theme.of(context).hintColor
+              : const Color(0xFFE7EBEE),
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: Icon(
+          _icons[index],
+          size: 25,
+          color: _selectedIndex == index
+              ? Theme.of(context).primaryColor
+              : const Color(0xFFB4C1C4),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: ListView(
           padding: const EdgeInsets.symmetric(vertical: 30),
-          children: const [
-            Padding(
+          children: [
+            const Padding(
               padding: EdgeInsets.only(left: 20, right: 120),
               child: Text(
                 "What would you like to find?",
@@ -22,7 +62,12 @@ class _HomeScreenState extends State<HomeScreen> {
                   fontWeight: FontWeight.bold,
                 ),
               ),
-            )
+            ),
+            const SizedBox(height: 20),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children:
+                    List.generate(_icons.length, (index) => _buildIcon(index)))
           ],
         ),
       ),
